f790ad815b535f4b0eebe94930c13b453ab0ef24
Михаил <mishafedoseev753@gmail.com>
2024-12-26 20:42:53 +0500

    Структурирование главной страницы

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,21 +2,51 @@ import React from "react"
 import {useGetChapters} from "../../shared/hooks";
 import Section from "../../widgets/section";
 import Chapter from "../../widgets/chapter";
+import "./styles.css"
 
 export default function HomePage(){
 
     const query = useGetChapters()
 
     return (
-        <div>
-            {query.isLoading ?
-                "Загрузка" :
-                query.isError ? "Ошибка!" : query?.data?.sections.map((section, index) => (
-                    <Section title={section.title} key={index}>
-                        {section.chapters.map((chapter, index) => (
-                            <Chapter key={index} title={chapter.title} chapterId={chapter.id} />
-                        ))}
-                    </Section>
-                ))}
+        <div className="container">
+            <div className="header">
+                <div className="label">
+                    <h2>Онлайн справка</h2>
+                </div>
+                <div className="navigation">
+                    <button>Создать главу</button>
+                    <button>Создать раздел</button>
+                    <button>Редактировать</button>
+                </div>
+            </div>
+            <div className="sections">
+                {query.isLoading ?
+                    "Загрузка" :
+                    query.isError ? "Ошибка!" : query?.data?.sections.map((section, index) => (
+                        <Section title={section.title}
+                                 key={index}
+                                 icon={section.icon}
+                                 createDate={section.createDate}
+                                 updateDate={section.updateDate}
+                                 isEnabled={section.isEnabled}
+                                 displayOrder={section.displayOrder}
+                        >
+                            {section.chapters.map((chapter, index) => (
+                                <Chapter
+                                    key={index}
+                                    title={chapter.title}
+                                    chapterId={chapter.id}
+                                    icon={chapter.icon}
+                                    isEnabled={chapter.isEnabled}
+                                    createDate={chapter.createDate}
+                                    updateDate={chapter.updateDate}
+                                    sectionId={chapter.sectionId}
+                                    displayOrder={chapter.displayOrder}
+                                />
+                            ))}
+                        </Section>
+                    ))}
+            </div>
         </div>)
 }
--- src/widgets/section/index.jsx
+++ src/widgets/section/index.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import Icon from "../../shared/ui/icon";
+import "./styles.css"
+import {formatDistanceToNow} from "date-fns";
+import {ru} from "date-fns/locale";
+import PublicStatus from "../../shared/ui/publicStatus";
 
 export default function Section(props) {
+
+    const lastDate = new Date(props.updateDate)
+    const date = formatDistanceToNow(lastDate, {locale: ru})
+
     return (
-        <div>
-            {props.title}
-            <br />
-            {props.children}
+        <div className="section">
+            <div className="section-container">
+                <div className="section-name">
+                    <div className="section-icon">
+                        <Icon name={props.icon}/>
+                    </div>
+                    <div className="section-title">
+                        {props.title}
+                    </div>
+                    <div className="section-status">
+                        <PublicStatus isEnabled={props.isEnabled} />
+                    </div>
+                </div>
+                <div className="section-history">
+                    {date}
+                </div>
+            </div>
+            <div className="chapters">
+                {props.children}
+            </div>
         </div>
     )
 }
